929df2f9f0eb041b8d299722cec1dcd2003ed1c4
AmandaRP <amanda@dekleineorchards.com>
2019-05-21 07:42:16 +0000

    Cleaned up code

--- 2019/week21/plastic_waste.R
+++ 2019/week21/plastic_waste.R
@@ -4,12 +4,8 @@ library(inspectdf)
 library(cowplot)
 library(ggrepel)
 
-#Ideas: 
-# 1. explore corrr package. 
-# 2. gganimate (for year)
-# 3. inspect df
 
-#Read data:
+#Read data and clean with janitor:
 coast_vs_waste <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-21/coastal-population-vs-mismanaged-plastic.csv") %>% clean_names()
 waste_vs_gdp <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-21/per-capita-plastic-waste-vs-gdp-per-capita.csv")  %>% clean_names()
 mismanaged_vs_gdp <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-21/per-capita-mismanaged-plastic-waste-vs-gdp-per-capita.csv")  %>% clean_names()
@@ -32,24 +28,21 @@ View(waste)
 View(waste)
 
 
-#Look for correlations:
-inspect_cor(waste, show_plot = TRUE) #pop and mismng_pl_waste_tons (.75) *****
-inspect_na(waste, show_plot = TRUE)
+#Look for correlations using the inspectdf package:
+inspect_cor(waste, show_plot = TRUE) 
+inspect_na(waste, show_plot = TRUE) #Looks like we only have 2010 waste data
 
-#Note, we only seem to have 2010 statistics. No gganimate today :(
-inspect_na(mismanaged_vs_gdp, show_plot = TRUE)
-
-#Richer nations produce more plastic waste
+#Plots:
 data4plot <- waste %>% filter(year == 2010) %>% filter(entity != "World")
 plt1 <- ggplot(data4plot, aes(x = log(gdp_per_cap), 
              y = log(plastic_waste_per_cap), 
              size = mismng_pl_waste_tons)) +
   geom_smooth(method = "lm") +
-  geom_point(color = "#333333") +
+  geom_point(color = "#333333", alpha = 0.7) +
   geom_text_repel(aes(label = entity),
     color         = "red",
-    size          = 3,
-    data          = subset(data4plot, log(plastic_waste_per_cap) > 0 | log(plastic_waste_per_cap) < -4.5 | log(gdp_per_cap) < 6.5 | code %in% c("USA","CHN")),
+    size          = 4,
+    data          = subset(data4plot, log(plastic_waste_per_cap) > 0 | log(plastic_waste_per_cap) < -4.5 | (log(gdp_per_cap) < 7 & log(plastic_waste_per_cap) < -4) | code %in% c("USA","CHN")),
     nudge_y       = .8,
     segment.size  = 0.2,
     segment.color = "grey50",
@@ -57,22 +50,20 @@ plt1 <- ggplot(data4plot, aes(x = log(gdp_per_cap),
   ) +
   labs(title = "Richer nations tend to produce more plastic waste",
        x = "GDP Per Capita (log scale)",
-       y = "Plastic Waste Per Capita (log scale)",
+       y = "Per Capita Plastic Waste in kg/day (log scale)",
        size = "Mismanagement of \nPlastic Waste (tons)")  +
-  theme(legend.position = "none",
-        plot.title = element_text(family = "Comic Sans MS")) 
-
+  theme(legend.position = "none") 
 
 
 #Richer nations manage their plastic waste better
 plt2 <- ggplot(data4plot, aes(x = log(gdp_per_cap), 
              y = mismng_pl_waste_per_cap, 
              size = mismng_pl_waste_tons)) +
-  geom_smooth() + 
-  geom_point(color = "#333333") + 
+  geom_smooth(show.legend = FALSE) + 
+  geom_point(color = "#333333", alpha = 0.7) + 
   geom_text_repel(aes(label = entity), 
     color         = "red",
-    size          = 3,
+    size          = 4,
     data          = subset(data4plot, mismng_pl_waste_per_cap > .14 | code %in% c("USA","CHN")),
     nudge_y       = .025,
     nudge_x       = .1,
@@ -81,37 +72,17 @@ plt2 <- ggplot(data4plot, aes(x = log(gdp_per_cap),
     direction     = "x"
   ) + 
   ylim(0, NA) +
-  labs(title = "...But countries having mid-range GDP are\nthe worst at management of their waste ",
+  labs(title = "Many countries having mid-range GDP are\nbad at management of their plastic waste ",
        x = "GDP Per Capita (log scale)",
-       y = "Mismanaged Plastic Waste Per Capita",
-       size = "Mismanagement of \nPlastic Waste (tons)") 
+       y = "Per Capita Mismanaged Plastic Waste in kg/day",
+       size = "Mismanaged\nPlastic Waste (tons)") 
 
+#Get the legend from plot 2 so that I can put it in its own plot grid panel:
 l <- get_legend(plt2) 
 
+#Final plot
 plot_grid(plt1, 
           plt2 + theme(legend.position = "none"), 
           l,
           nrow = 1)
 
-
-# Diverging lolly pop
-
-waste_no_world <- waste %>% filter(entity != "World")
-x <- waste_no_world$mismng_pl_waste_tons
-waste_no_world$mmg_z <- round((x - mean(x, na.rm = TRUE))/sd(x, na.rm = TRUE), 2) 
-waste_no_world$entity <- factor(waste_no_world$entity, levels = unique(waste_no_world$entity)) 
-waste %>%
-  filter(year == 2010 & !is.na(mmg_z)) %>%
-  #head() %>%
-  ggplot(aes(x=entity, y=mmg_z, label=mmg_z)) + 
-  geom_point(stat='identity', fill="black", size=6)  +
-  geom_segment(aes(y = 0, 
-                   x = entity, 
-                   yend = mmg_z, 
-                   xend = entity)) +
-  #geom_text(color="white", size=2) +
-  #labs(title="Diverging Lollipop Chart", 
-  #     subtitle="Normalized mileage from 'mtcars': Lollipop") + 
-  #ylim(-2.5, 2.5) +
-  coord_flip() +
-  theme_bw()
