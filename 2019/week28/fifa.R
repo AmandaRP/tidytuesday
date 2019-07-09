a5e1ff5613d2b471661e3e1bd58458185daeecda
AmandaRP <amanda@dekleineorchards.com>
2019-07-09 04:00:05 +0000

    new week. new script.

---
+++ 2019/week28/fifa.R
@@ -0,0 +1,8 @@
+
+wwc_outcomes <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-07-09/wwc_outcomes.csv")
+squads <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-07-09/squads.csv")
+codes <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-07-09/codes.csv")
+
+dplyr::left_join(wwc_outcomes, codes, by = "team")
+
+#chord diagram between countries?
